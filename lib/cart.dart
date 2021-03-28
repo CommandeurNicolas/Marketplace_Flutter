162afd7779880cba384f5ec4e20f827081bb9c71
Nicolas.C <comcomnico@gmail.com>
2021-03-29 00:59:58 +0200

    cart page + transparent background images + minor changes

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,5 +1,11 @@
+import 'dart:collection';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:market_place_test/product_model.dart';
+import 'package:material_dialogs/material_dialogs.dart';
+import 'package:material_dialogs/widgets/buttons/icon_button.dart';
+import 'package:material_dialogs/widgets/buttons/icon_outline_button.dart';
 
 import 'constants.dart';
 import 'custom_appbar.dart';
@@ -15,12 +21,23 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
   double _total = 0.0;
+  HashMap<Product, int> _productNumber = new HashMap<Product, int>();
+
   @override
-  Widget build(BuildContext context) {
+  void initState() {
     for (Product item in widget.products) {
       _total += double.parse(item.price);
+      _productNumber.containsKey(item)
+          ? _productNumber[item]++
+          : _productNumber[item] = 1;
     }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: CustomAppbar(
         title: "Your Cart",
       ),
@@ -30,51 +47,102 @@ class _CartPageState extends State<CartPage> {
             child: ListView.builder(
               scrollDirection: Axis.vertical,
               shrinkWrap: true,
-              itemCount: widget.products.length,
+              itemCount: _productNumber.length,
               itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+                return Padding(
+                  padding: const EdgeInsets.all(24.0),
                   child: Container(
-                    padding: EdgeInsets.all(15.0),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
                       children: [
-                        // Image.network(p.image),
-
                         Container(
-                          padding: EdgeInsets.only(left: 2.0, right: 16.0),
-                          child: Image.asset(
-                            "assets/${widget.products[index].image}",
-                            height: 100,
-                            width: 100,
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              color: Color(0xffcacaca),
+                              borderRadius: BorderRadius.circular(20.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Image.asset(
+                                "assets/${_productNumber.keys.elementAt(index).image}"),
                           ),
                         ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.products[index].name.toUpperCase(),
-                              style: TextStyle(fontSize: 15.0),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Container(
-                              width: 60.0,
-                              child: Divider(
-                                height: 10,
-                                thickness: 2.0,
-                                indent: 10.0,
-                                color: secondary,
+                        Expanded(
+                          child: Container(
+                            height: 100,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 18.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    _productNumber.keys
+                                            .elementAt(index)
+                                            .name[0]
+                                            .toUpperCase() +
+                                        _productNumber.keys
+                                            .elementAt(index)
+                                            .name
+                                            .substring(1),
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  Text(
+                                    (double.parse(_productNumber.keys
+                                                .elementAt(index)
+                                                .price))
+                                            // * _productNumber.values
+                                            //   .elementAt(index))
+                                            .toString() +
+                                        " \$",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
                             ),
-                            Center(
-                              child: Text(
-                                "${widget.products[index].price} \$",
-                                style: TextStyle(fontSize: 16.0),
+                          ),
+                        ),
+                        Container(
+                          height: 100,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                // width: 40.0,
+                                child: ElevatedButton(
+                                  onPressed: () => _showDialog(
+                                    _productNumber.keys.elementAt(index),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: CircleBorder(),
+                                    padding: EdgeInsets.all(0.0),
+                                    primary: Colors.red,
+                                  ),
+                                  child: Icon(Icons.delete_outline),
+                                ),
                               ),
-                            )
-                          ],
+                              Container(
+                                // width: 40.0,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    shape: CircleBorder(),
+                                    padding: EdgeInsets.all(0.0),
+                                    primary: primary.withOpacity(0.6),
+                                  ),
+                                  child: Text(
+                                    _productNumber.values
+                                        .elementAt(index)
+                                        .toString(),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
@@ -83,33 +151,143 @@ class _CartPageState extends State<CartPage> {
               },
             ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              color: Colors.white,
-              height: 70.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Total"),
-                      Text(
-                        _total.toString(),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "Total :",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
+                    ),
+                    Text(
+                      _total.toString() + " \$",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text("+ shipping fee"),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 20,
+                ),
+                _buildCheckoutButton(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _showDialog(Product p) {
+    return Dialogs.materialDialog(
+      msg: 'Are you sure ? you can\'t undo this',
+      title: "Delete",
+      color: Colors.white,
+      context: context,
+      actions: [
+        IconsOutlineButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          text: 'Cancel',
+          iconData: Icons.cancel_outlined,
+          textStyle: TextStyle(color: Colors.grey),
+          iconColor: Colors.grey,
+        ),
+        IconsButton(
+          onPressed: () {
+            setState(() {
+              _total -= _productNumber[p] * double.parse(p.price);
+              _productNumber.remove(p);
+            });
+            Navigator.pop(context);
+          },
+          text: "Delete",
+          iconData: Icons.delete,
+          color: Colors.red,
+          textStyle: TextStyle(color: Colors.white),
+          iconColor: Colors.white,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTotalText() {}
+
+  Widget _buildCheckoutButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(primary),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+          EdgeInsets.all(0.0),
+        ),
+      ),
+      child: Container(
+        height: 50,
+        width: 250,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 0.0),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Opacity(
+                    opacity: 0.5,
+                    child: Container(
+                      height: 32,
+                      width: 32,
+                      decoration: BoxDecoration(
+                        color: Color(0xffCCCCCC),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Go to checkout"),
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Colors.white,
+                    size: 24,
                   )
                 ],
               ),
             ),
-          )
-        ],
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 40.0),
+                child: Text(
+                  "Checkout",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
